f2272cd4dede3b1bcdbe859be639dea214361f2c
dickinsonre <robert.dickinson@gmail.com>
2023-03-09 08:51:09 -0500

    added bat files

---
+++ VBA_MSDOS/appendSWMM5Files.bat
@@ -0,0 +1,2 @@
+copy SWMM5_RNF\*.inp + SWMM5_HDR\*.inp swmm5_combined.inp
+pause
---
+++ VBA_MSDOS/rename_subFolders.bat
@@ -0,0 +1,6 @@
+ren swmm5_hdr\*.inp *_swmm5_hdr.inp
+ren swmm5_san\*.inp *_swmm5_san.inp
+ren swmm5_rnf\*.inp *_swmm5_rnf.inp
+ren xpx_hdr\*.xpx *_xpx_hdr.xpx
+ren xpx_rnf\*.xpx *_xpx_rnf.xpx
+pause
