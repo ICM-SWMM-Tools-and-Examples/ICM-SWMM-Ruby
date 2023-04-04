a95e7912b8b7730a151380f4fd808fdb0a93992e
dickinsonre <robert.dickinson@gmail.com>
2023-04-04 10:50:57 +0800

    more md

--- ICM_ODIC CFG Files_for_InfoSewer/PandocMD.bat
+++ ICM_ODIC CFG Files_for_InfoSewer/PandocMD.bat
@@ -1,2 +1,2 @@
-pandoc  "Converting InfoSewer Model to ICM InfoWorks Network.txt" -t markdown -o "Converting InfoSewer Model to ICM InfoWorks Network.md" 
+pandoc --wrap=none "Converting InfoSewer Model to ICM InfoWorks Network.txt" -t markdown -o "Converting InfoSewer Model to ICM InfoWorks Network.md" 
 pause
